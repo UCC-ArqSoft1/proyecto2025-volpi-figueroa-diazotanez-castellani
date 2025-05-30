a1a6d108346fcb513ecc44d935e0195d88dee1e8
Franco Castellani <114883365+francocaste2705@users.noreply.github.com>
2025-05-30 18:00:35 -0300

    ultimo
    aa

--- backend/models/inscripcion.go
+++ backend/models/inscripcion.go
@@ -2,11 +2,12 @@ package models
 
 import "time"
 
-type Usuario struct {
-	ID          uint      `gorm:"primaryKey;autoIncrement"` // Cambiado de UsuarioID a ID para mayor consistencia
-	UsuarioID   uint      `gorm:"primaryKey;autoIncrement"`
-	ActividadID uint      `gorm:"not null"` // ID de la actividad a la que se inscribe
-	Fecha       time.Time `gorm:"not null"` // Fecha de inscripción
+type Inscripcion struct {
+	ID          uint      `gorm:"primaryKey;autoIncrement"` // ID único de la inscripción
+	UsuarioID   uint      `gorm:"not null"`                 // ID del usuario que se inscribe
+	ActividadID uint      `gorm:"not null"`                 // ID de la actividad a la que se inscribe
+	Fecha       time.Time `gorm:"not null"`                 // Fecha de inscripción
+
 	//Relaciones
 	Usuario   Usuario   `gorm:"foreignKey:UsuarioID;references:ID"`   // Relación con el usuario que se inscribe
 	Actividad Actividad `gorm:"foreignKey:ActividadID;references:ID"` // Relación con la actividad a la que se inscribe
